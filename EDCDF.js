4fcc594505c029e913c15a9365c0ceeb29ffa821
Mariana-JL <61875840+Mariana-JL@users.noreply.github.com>
2020-05-09 15:12:53 -0500

    Add files via upload

---
+++ EDCDF.js
@@ -0,0 +1,7 @@
+var inputNum =Number(prompt('Ingresa un número entero mayor de 10 y menor de 20'));
+if((inputNum <10) ||  (inputNum > 20)){
+    alert('Ese número no es mayor de 10 ni menor de 20');
+
+}else{
+    alert('Correcto, el número es valido');    }
+document.write('<h1>Hola Mundo</h1>');
